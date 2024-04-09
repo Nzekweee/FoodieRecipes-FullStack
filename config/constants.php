89297f04600e9764df7ac5234a393a05b2c15ab9
Nzekweee <157707013+Nzekweee@users.noreply.github.com>
2024-04-09 16:40:39 +0100

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,7 +1,11 @@
 <?php
+//if the user is not logged in redirect to home page
 session_start();
-define('ROOT_URL', 'http://localhost/foodierecipes/');
-define ('DB_HOST', 'localhost');
-define('DB_USER','Nzekweee');
-define('DB_PASS', 'nzekweemma1234');
-define('DB_NAME', 'foodierecipe');
+define('ROOT_URL', 'http://foodierecipes.42web.io/');
+define ('DB_HOST', 'sql306.infinityfree.com');
+define('DB_USER','if0_36333923');
+define('DB_PASS', 'bUTMLQCl1T');
+define('DB_NAME', 'if0_36333923_foodierecipe');
+
+
+///please replace the config/constants.php file with this  both in the root directory and dashboard/config/constants.php folder also remove the comments ejor 
