36f478a8789559c4f36f50ad13ae80bcffa76cd5
Nzekweee <157707013+Nzekweee@users.noreply.github.com>
2024-04-09 16:41:54 +0100

    Update constants.php

--- dashboard/config/constants.php
+++ dashboard/config/constants.php
@@ -1,8 +1,9 @@
 <?php
 //if the user is not logged in redirect to home page
 session_start();
-define('ROOT_URL', 'http://localhost/foodierecipes/');
-define ('DB_HOST', 'localhost');
-define('DB_USER','Nzekweee');
-define('DB_PASS', 'nzekweemma1234');
-define('DB_NAME', 'foodierecipe');
+define('ROOT_URL', 'http://foodierecipes.42web.io/');
+define ('DB_HOST', 'sql306.infinityfree.com');
+define('DB_USER','if0_36333923');
+define('DB_PASS', 'bUTMLQCl1T');
+define('DB_NAME', 'if0_36333923_foodierecipe');
+
